01fd7739ddcf551a54aea98a0361bcc49651ae7d
Charli Sung <charlisung@gmail.com>
2021-08-10 09:00:46 -0700

    add  layout, navbar, home index styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
 import { FaAmericanSignLanguageInterpreting } from "react-icons/fa";
 import '../styles/global.css'
 
 
 export default function Navbar() {
+
+    const [navbar, setNavbar] = useState(false)
+
+    const changeBackground = () => {
+        if (window.scrollY >= 50) {
+            setNavbar(true)
+        } else {
+            setNavbar(false)
+        }
+    }
+
+    if (typeof window !== 'undefined') {
+        window.addEventListener('scroll', changeBackground)
+    }
+
     return (
-        <nav className="navbar">
-            <div className="container"> 
+        <nav className={navbar ? 'navbar active' : 'navbar'}>
+            <div className="container">
                 <h2 className="logo-title">
                     <Link to="/">
-                    <FaAmericanSignLanguageInterpreting className="icon" />
-                    SLP Bell
+                        <FaAmericanSignLanguageInterpreting className="icon" />
+                        SLP Bell
                     </Link>
                 </h2>
-            <div className="links">
-                <Link to="/blog">Blog</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/about">About</Link>
-                <Link to="/tags">Tags</Link>
-            </div>
+                <div className="links">
+                    <Link to="/blog">blogs</Link>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/tags">Tags</Link>
+                </div>
             </div>
         </nav>
     )
