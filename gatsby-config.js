b954ca040dd914d5ae3f8efdfd0e456d9e52f12c
Charli Sung <charlisung@gmail.com>
2021-08-09 18:44:43 -0700

    add markdown files, images

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,11 +10,26 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blogs`,
+        path: `${__dirname}/src/blogs/`,
       },
     },
   ],
