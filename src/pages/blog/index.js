0e26aba65672d5101f1dd170a89740f52bbb91e3
Charli Sung <charlisung@gmail.com>
2021-08-11 10:46:16 -0700

    add category function

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,12 +1,64 @@
-import { graphql } from "gatsby"
-import React from "react"
+import { graphql, Link } from "gatsby"
+import React, { useState } from "react"
 import Layout from "../../components/Layout"
-import BlogItems from "../../templates/blog-posts-page"
+// import BlogItems from "../../templates/blog-posts-page"
 
 const Blog = ({ data }) => {
+  const allPosts = data.allMarkdownRemark.edges
+
+  const getCategories = items => {
+    let categoryItems = items.map(item => {
+      return item.node.frontmatter.category
+    })
+    let uniqueCategories = new Set(categoryItems)
+    let categories = Array.from(uniqueCategories)
+    categories = ["All posts", ...categories]
+    return categories
+  }
+
+  const [categories, setCategories] = useState(getCategories(allPosts))
+  const [state, setState] = {
+    items: data.allMarkdownRemark.edges,
+    blogPostItems: data.allMarkdownRemark.edges,
+    categories: getCategories(data.allMarkdownRemark.edges),
+  }
+
+  const handleClick = category => {
+    if (category === "All posts") {
+      setState({
+        blogPostItems: [...data.allMarkdownRemark.edges],
+      })
+    } else {
+      setState({
+        blogPostItems: [
+          data.allMarkdownRemark.edges.fiter(edge => {
+            return edge.node.frontmatter.category === category
+          }),
+        ],
+      })
+    }
+  }
+
   return (
     <Layout>
-      <BlogItems items={data} />
+      h3llo
+      {categories.map((category, i) => {
+        return (
+          <button type="button" key={i} onClick={handleClick}>
+            {category}
+          </button>
+        )
+      })}
+      <div>
+        {allPosts.map((post, i) => {
+          return (
+            <Link key={i}>
+              <div>{post.node.frontmatter.title}</div>
+            </Link>
+          )
+        })}
+      </div>
+      {/* <BlogItems items={data} /> */}
     </Layout>
   )
 }
